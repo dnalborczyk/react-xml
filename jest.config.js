4a9ee34da7ba4e9d5029ec37248af583e3139b77
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-06-26 14:56:13 -0400

    Modify jest config to ignore bigint tests on unsupported platforms

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 'use strict' // eslint-disable-line
 
-module.exports = {}
+const { satisfies } = require('semver')
+
+const testPathIgnorePatterns = []
+
+// bigint is supported with node.js v10.4.0
+const canTest = satisfies(process.version, '>=10.4.0')
+
+if (!canTest) {
+  testPathIgnorePatterns.push('bigint')
+}
+
+module.exports = {
+  testPathIgnorePatterns,
+}
