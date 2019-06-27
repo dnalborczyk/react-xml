03b5f3b9b4d0f10038e0738e9eb474f52254150d
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-06-27 09:51:51 -0400

    Add @babel/plugin-transform-flow-strip-types

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@
 
 const plugins = [
   '@babel/plugin-syntax-bigint',
+  '@babel/plugin-transform-flow-strip-types',
   '@babel/plugin-transform-modules-commonjs',
   ['@babel/plugin-transform-react-jsx', { throwIfNamespace: false }],
 ]
