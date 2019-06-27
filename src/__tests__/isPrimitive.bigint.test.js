d0c793cb88edaa41433131c1aba1a92af3fe29e9
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-06-27 14:29:57 -0400

    Remove describe wrapper for non-existing utils

--- src/__tests__/isPrimitive.bigint.test.js
+++ src/__tests__/isPrimitive.bigint.test.js
@@ -2,14 +2,12 @@ import isPrimitive from '../isPrimitive.js'
 
 // NOTE: bigint tests can be merged with utils.test.js file once
 // node.js v8.x support drops
-describe('utils tests', () => {
-  describe('isPrimitive tests', () => {
-    test.each`
-      param | expected | type
-      ${1n} | ${true}  | ${'BigInt'}
-    `('should return $expected when $type is passed', ({ param, expected }) => {
-      const result = isPrimitive(param)
-      expect(result).toEqual(expected)
-    })
+describe('isPrimitive tests', () => {
+  test.each`
+    param | expected | type
+    ${1n} | ${true}  | ${'BigInt'}
+  `('should return $expected when $type is passed', ({ param, expected }) => {
+    const result = isPrimitive(param)
+    expect(result).toEqual(expected)
   })
 })
--- src/__tests__/isPrimitive.test.js
+++ src/__tests__/isPrimitive.test.js
@@ -1,21 +1,19 @@
 import isPrimitive from '../isPrimitive.js'
 
-describe('utils tests', () => {
-  describe('isPrimitive tests', () => {
-    test.each`
-      param                | expected | type
-      ${true}              | ${true}  | ${'Boolean'}
-      ${0}                 | ${true}  | ${'Number'}
-      ${''}                | ${true}  | ${'String'}
-      ${[]}                | ${false} | ${'Array'}
-      ${{}}                | ${false} | ${'Object'}
-      ${Promise.resolve()} | ${false} | ${'Promise'}
-      ${Symbol('')}        | ${false} | ${'Symbol'}
-      ${null}              | ${false} | ${'null'}
-      ${undefined}         | ${false} | ${'undefined'}
-    `('should return $expected when $type is passed', ({ param, expected }) => {
-      const result = isPrimitive(param)
-      expect(result).toEqual(expected)
-    })
+describe('isPrimitive tests', () => {
+  test.each`
+    param                | expected | type
+    ${true}              | ${true}  | ${'Boolean'}
+    ${0}                 | ${true}  | ${'Number'}
+    ${''}                | ${true}  | ${'String'}
+    ${[]}                | ${false} | ${'Array'}
+    ${{}}                | ${false} | ${'Object'}
+    ${Promise.resolve()} | ${false} | ${'Promise'}
+    ${Symbol('')}        | ${false} | ${'Symbol'}
+    ${null}              | ${false} | ${'null'}
+    ${undefined}         | ${false} | ${'undefined'}
+  `('should return $expected when $type is passed', ({ param, expected }) => {
+    const result = isPrimitive(param)
+    expect(result).toEqual(expected)
   })
 })
